e5afbdad0941e64737c2ab08a5ea2f6f9fa74c33
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-10 13:46:17 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(name="squawk!", squawk)
+def parrot(name="squawk!")
   puts name
-  return squawk
 end 
-
-parrot
+return squawk
